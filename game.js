926c8c31ca46dd95fadd7c8c1a06712637ee6083
justin-xia15 <83360815+justin-xia15@users.noreply.github.com>
2021-07-16 15:00:56 -0400

    Added player turn function

--- game.js
+++ game.js
@@ -1,15 +1,23 @@
-const square1 = document.getElementById("1");
-const square2 = document.getElementById("2");
-const square3 = document.getElementById("3");
-const square4 = document.getElementById("4");
-const square5 = document.getElementById("5");
-const square6 = document.getElementById("6");
-const square7 = document.getElementById("7");
-const square8 = document.getElementById("8");
-const square9 = document.getElementById("9");
+const square1 = document.getElementById("0");
+const square2 = document.getElementById("1");
+const square3 = document.getElementById("2");
+const square4 = document.getElementById("3");
+const square5 = document.getElementById("4");
+const square6 = document.getElementById("5");
+const square7 = document.getElementById("6");
+const square8 = document.getElementById("7");
+const square9 = document.getElementById("8");
 
-let player1Moves = [];
-let player2Moves = [];
-let winConditions = ["123", "321", "456", "654", "789", "987", "147", "741",
-  "258", "852", "369", "963", "159", "951", "357", "753"
-];
+
+let gameState = ["", "", "",
+                 "", "", "",
+                 "", "", ""];
+
+function playerTurn() {
+for (let i = 0; i < 9; i++) {
+  document.querySelectorAll(".box")[i].addEventListener("click", function() {
+    gameState[this.innerText - 1] = "X";
+    this.innerText = "X";
+  });
+}
+}
