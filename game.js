62465b0b39eccaa9ed8b122c2f28f207e94cdd3d
justin-xia15 <83360815+justin-xia15@users.noreply.github.com>
2021-07-16 14:20:26 -0400

    Added square id's and win conditions

---
+++ game.js
@@ -0,0 +1,15 @@
+const square1 = document.getElementById("1");
+const square2 = document.getElementById("2");
+const square3 = document.getElementById("3");
+const square4 = document.getElementById("4");
+const square5 = document.getElementById("5");
+const square6 = document.getElementById("6");
+const square7 = document.getElementById("7");
+const square8 = document.getElementById("8");
+const square9 = document.getElementById("9");
+
+let player1Moves = [];
+let player2Moves = [];
+let winConditions = ["123", "321", "456", "654", "789", "987", "147", "741",
+  "258", "852", "369", "963", "159", "951", "357", "753"
+];
